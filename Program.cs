1616b7641ec10e735e5a87b27046c3cde74458ec
Dsulli43 <126179726+Dsulli43@users.noreply.github.com>
2023-03-28 09:37:58 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumericTypes;
+public class Program
+  {
+        public static void Main()
+    
+        {
+         
+           
+            
+            Console.WriteLine("What is the the length");
+           double Length = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the width");
+           double Width = Convert.ToInt32(Console.ReadLine());
+        
+           double area = Length * Width;  
+         Console.WriteLine("This is the area of the rectangle: " + area);
+           Console.ReadKey();
+        }
+
+    }
+    
